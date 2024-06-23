48a84d17369fd79044e34dd4cd6f8d10c76c0bc0
zotel1 <zotelsigel@gmail.com>
2024-06-22 21:27:24 -0300

    update menu

--- components/shared/main-menu.tsx
+++ components/shared/main-menu.tsx
@@ -1,7 +1,7 @@
 import { cn } from "@/libs/utils";
 import Link from "next/link";
 
-import { RiHome3Line } from "react-icons/ri";
+import { RiHome3Line, RiBriefcase2Line } from "react-icons/ri";
 const MainMenu = () => {
 
     const routes = [
@@ -9,12 +9,17 @@ const MainMenu = () => {
             label: 'Home',
             icon: RiHome3Line,
             href: '/',
+        },
+        {
+            label: 'Work',
+            icon: RiBriefcase2Line,
+            href: '/work'
         }
     ];
     return ( <ul>
         <li>
             {routes.map((route) => (
-            <Link key={route.href} href={route.href} className={cn('flex items-center gap-4 text-gray-500')}>
+            <Link key={route.href} href={route.href} className={cn('flex items-center gap-4 text-gray-500 p-5 border-b border-gray-500/30')}>
             <route.icon size={18}/>
             {route.label}
             </Link>
