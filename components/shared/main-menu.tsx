8d53d5d3a051b5a7c121f29ecc8da1babb50238b
zotel1 <zotelsigel@gmail.com>
2024-06-22 21:16:11 -0300

    update:menu dinamico

--- components/shared/main-menu.tsx
+++ components/shared/main-menu.tsx
@@ -3,11 +3,22 @@ import Link from "next/link";
 
 import { RiHome3Line } from "react-icons/ri";
 const MainMenu = () => {
+
+    const routes = [
+        {
+            label: 'Home',
+            icon: RiHome3Line,
+            href: '/',
+        }
+    ];
     return ( <ul>
         <li>
-            <Link href='/' className={cn('flex items-center gap-4 text-gray-500')}>
-            <RiHome3Line size={18}/>
-            Home</Link>
+            {routes.map((route) => (
+            <Link key={route.href} href={route.href} className={cn('flex items-center gap-4 text-gray-500')}>
+            <route.icon size={18}/>
+            {route.label}
+            </Link>
+            ))}
         </li>
     </ul>
     );
