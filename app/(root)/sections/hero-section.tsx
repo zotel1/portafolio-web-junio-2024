26983fdabb3c8947dd0b51435b221175f30392ca
zotel1 <zotelsigel@gmail.com>
2024-06-23 15:10:33 -0300

    update

---
+++ app/(root)/sections/hero-section.tsx
@@ -0,0 +1,41 @@
+import { information } from '@/assets/data';
+import ButtonSocialMedia from "@/components/ui/button-social-media";
+import Image from "next/image";
+
+
+export const HeroSection = () => {
+    return (
+        <section id='home'>
+            <div className="space-y-10">
+                <div className="relative w-24 h-24">
+                    <Image
+                        src={information.photo}
+                        alt='Imagen'
+                        layout='responsive'
+                        width={200}
+                        height={200}
+                        className="object-cover rounded-xl grayscale"
+                    />
+                </div>
+
+                <div className="max-w-3xl">
+                    <h1 className="text-4xl  text-white mb-5">
+                        {`Hey, me llamo Cristian - Soy un desarrollador web en formación`}
+                    </h1>
+                    <p className="text-gray-500">
+                        Desarrollador web desde hace un año y medio
+                    </p>
+                </div>
+                <div>
+                    {information.socialMedia.map((social) => (
+                        <ButtonSocialMedia
+                            key={social.href}
+                            href={social.href}
+                            icon={social.icon}
+                        />
+                    ))}
+                </div>
+            </div>
+        </section>
+    )
+}
--- components/shared/main-menu.tsx
+++ components/shared/main-menu.tsx
@@ -1,5 +1,6 @@
 import { cn } from "@/libs/utils";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 import { RiHome3Line, RiBriefcase2Line, RiShapeLine, RiUserLine, RiMailLine } from "react-icons/ri";
 const MainMenu = () => {
@@ -31,14 +32,20 @@ const MainMenu = () => {
         href: '/contacto'
         }
     ];
-    return ( <ul>
+
+const pathname = usePathname();
+
+    return ( 
+    <ul>
         <li>
             {routes.map((route) => (
             <Link 
             key={route.href} 
             href={route.href} 
             className={cn(
-                'flex items-center gap-4 text-gray-500 py-5 px-8 border-b border-gray-500/30 hover:bg-gray-500/5 hover:text-white transition-colors duration-300')}>
+                'flex items-center gap-4 text-gray-500 py-5 px-8 border-b border-gray-500/30 hover:bg-gray-500/5 hover:text-white transition-colors duration-300',
+                pathname === route.href && 'text-white'
+                )}>
             <route.icon size={18}/>
             {route.label}
             </Link>
