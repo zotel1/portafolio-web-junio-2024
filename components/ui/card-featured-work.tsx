0cbeb4a6592f9dbbad8c3fee4b02335a148747ce
zotel1 <zotelsigel@gmail.com>
2024-06-23 23:51:07 -0300

    update

--- components/ui/card-featured-work.tsx
+++ components/ui/card-featured-work.tsx
@@ -9,7 +9,7 @@ const CardFeaturedWork = () => {
                 alt='Image' 
                 fill 
                 sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-                className='object-cover rounded-xl group-hover:scale-110'
+                className='object-cover rounded-xl group-hover:scale-110 transition-all duration-300'
                 />
             </div>
             <h5 className='text-gray-500 uppercase tracking-widest font-medium'>Landing Page</h5>
