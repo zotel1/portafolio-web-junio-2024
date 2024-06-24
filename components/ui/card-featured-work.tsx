0c72d8705829a8300246fc1bb24a86c61aca2147
zotel1 <zotelsigel@gmail.com>
2024-06-24 11:23:17 -0300

    update: card-featured-work-dinamico

--- components/ui/card-featured-work.tsx
+++ components/ui/card-featured-work.tsx
@@ -1,19 +1,38 @@
 import Image from "next/image";
 import Link from "next/link";
-const CardFeaturedWork = () => {
+
+interface CardFeaturedWorProps{
+    href: string;
+    image: string;
+    category: string;
+    title: string;
+}
+
+const CardFeaturedWork = ({
+    href, 
+    image, 
+    category, 
+    title,
+}: CardFeaturedWorProps) => {
     return (
-        <Link href='/' className='p-5 group'>
-            <div className='relative w-full h-80 mb-5'>
+        <Link href={href} className='p-5 group'>
+            <div className='relative w-full h-80 mb-5 overflow-hidden rounded-xl'>
                 <Image 
-                src='/landing-page.jpeg' 
+                src={image} 
                 alt='Image' 
                 fill 
                 sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
                 className='object-cover rounded-xl group-hover:scale-110 transition-all duration-300'
                 />
             </div>
-            <h5 className='text-gray-500 uppercase tracking-widest font-medium'>Landing Page</h5>
-            <h3 className='text-xl text-white'>Landing Page Digital Nao</h3>
+            <h5 className='text-gray-500 uppercase tracking-widest font-medium'>
+                Landing Page
+                {category}
+                </h5>
+            <h3 className='text-xl text-white group-hover:text-primary transition-colors duration-300'>
+                Landing Page Digital Nao
+                {title}
+                </h3>
         </Link>
     )
 }
